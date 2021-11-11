ee24abafcabac9b20d9a512feb636cbce883e4d1
Ana Spiko <91209101+anaspiko@users.noreply.github.com>
2021-11-11 13:33:28 +0100

    Delete entrypoint.py

--- entrypoint.py
+++
@@ -1,9 +0,0 @@
-import request
-import sys
-
-SERVICE1_URL = "http://service.example.com:8080"
-
-message = requests.get(sys.stdin.readline()).text
-data = ["md5", message]
-
-print(requests.post(SERVICE1_URL, data="\n".join(data)))
