ee09162d75ff0a913e7037c83a7e549f3603cdf7
Ana Spiko <91209101+anaspiko@users.noreply.github.com>
2021-11-11 14:02:43 +0100

    Add files via upload

--- entrypoint.py
+++ entrypoint.py
@@ -1,9 +1,18 @@
-import request
+import requests
 import sys
+import os
 
-SERVICE1_URL = "http://service.example.com:8080"
+SERVICE1_URL = "http://servis1:8081"
 
-message = requests.get(sys.stdin.readline()).text
+message = sys.stdin.readline()
+print("Skidam podatke s weba: " + message)
+
+userDefinedService = os.environ.get('SERVICE1')
+if userDefinedService:
+    SERVICE1_URL = userDefinedService
+
+print("Šaljem podatke na: " + SERVICE1_URL)
+message = requests.get(message).text
 data = ["md5", message]
 
 print(requests.post(SERVICE1_URL, data="\n".join(data)))
